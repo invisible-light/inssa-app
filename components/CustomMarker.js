5f50a693919a99688b0a596d91c7f13e282decb6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-11-12 23:06:22 +0900

    feat: custom marker

---
+++ components/CustomMarker.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faWalking} from '@fortawesome/free-solid-svg-icons';
+
+export default function CustomMarker({selected = false}) {
+  const primaryColor = '#7048E8';
+  const Container = styled.View`
+    padding: 8px;
+    background-color: ${selected ? 'white' : primaryColor};
+    border-radius: ${58 / 2};
+    border: 5px solid ${selected ? primaryColor : '#845EF7'};
+  `;
+
+  return (
+    <Container>
+      <FontAwesomeIcon
+        size={32}
+        icon={faWalking}
+        color={selected ? primaryColor : 'white'}
+      />
+    </Container>
+  );
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import styled from 'styled-components/native';
+
+import CustomMarker from '../components/CustomMarker';
 
 const styles = StyleSheet.create({
   map: {
@@ -26,7 +28,32 @@ export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pins: [],
+      markers: [
+        {
+          type: 'walk',
+          selected: false,
+          coordinate: {
+            latitude: 37.3418931,
+            longitude: 126.8315179,
+          },
+        },
+        {
+          type: 'walk',
+          selected: true,
+          coordinate: {
+            latitude: 37.3419526,
+            longitude: 126.8293454,
+          },
+        },
+        {
+          type: 'walk',
+          selected: false,
+          coordinate: {
+            latitude: 37.340312,
+            longitude: 126.8277398,
+          },
+        },
+      ],
     };
   }
 
@@ -41,8 +68,13 @@ export default class HomeScreen extends Component {
             longitude: 126.8293454,
             latitudeDelta: 0.005,
             longitudeDelta: 0.005,
-          }}
-        />
+          }}>
+          {this.state.markers.map((marker, idx) => (
+            <Marker coordinate={marker.coordinate} key={idx}>
+              <CustomMarker type={marker.type} selected={marker.selected} />
+            </Marker>
+          ))}
+        </MapView>
       </Container>
     );
   }
